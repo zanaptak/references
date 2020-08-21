ddb10ffa79e5b9ea05530b8ae27dc5d1766b4230
zanaptak <53196138+zanaptak@users.noreply.github.com>
2020-08-21 16:04:58 -0500

    .

--- App.fs
+++ App.fs
@@ -1,19 +1,8 @@
 module App
 
-(**
- The famous Increment/Decrement ported from Elm.
- You can find more info about Elmish architecture and samples at https://elmish.github.io/
-*)
-
-open Elmish
-open Elmish.React
 open Model
 open Msg
 open View
 open Update
 
-// App
-Program.mkSimple init update view
-|> Program.withReactSynchronous "elmish-app"
-|> Program.withConsoleTrace
-|> Program.run
+let a = (init,update,view)
--- View.fs
+++ View.fs
@@ -1,7 +1,5 @@
 module View
 
-open Fable.React
-open Fable.React.Props
 open Model
 open Msg
 
@@ -9,7 +7,6 @@ let view (model:Model) dispatch =
 
   let v: Data = model.Value
 
-  div []
-      [ button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
-        div [] [ str (string v) ]
-        button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] ]
+  dispatch Increment
+  dispatch Decrement
+  ()
